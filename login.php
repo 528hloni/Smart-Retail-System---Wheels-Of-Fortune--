828b0f3da6afd953bc94a76a2f45eea66877c843
Hloni <hloniletsaba@gmail.com>
2025-10-22 12:34:26 +0200

    first commit

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Wheels Of Fortune </h1>
+    <br><br>
+    <h3> Where Every Wheel Is A Win! </h3>
+    <br><br><br>
+    <form method="POST">
+        <p> Login to your account </p>
+        <br>
+        <label for="email"> Email </label>
+        <input type="email" id="email" name="email" placeholder="your@gmail.com" required>
+        <br>
+        <label for="password">Password </label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <input type="submit" value="Login">
+        <br>
+        <p>Do not have an account? <a href ="register.php">Register </a> </p>
+    </form>
+</body>
+</html>
